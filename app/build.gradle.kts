b3f3dc7e6d96f66e7a9c9f692ccb603bd9c27fa4
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-12-07 00:45:02 +0700

    init setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "paba.b.room"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "paba.b.room"
-        minSdk = 33
+        minSdk = 34
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -45,4 +46,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.runtime)
 }
---
+++ app/src/main/java/paba/b/room/database/daftarBelanja.kt
@@ -0,0 +1,24 @@
+package paba.b.room.database
+import android.content.ClipData.Item
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class daftarBelanja(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "Id")
+    var id: Int = 0,
+
+    @ColumnInfo(name="tanggal")
+    var tanggal: String? = null,
+
+    @ColumnInfo(name = "item")
+    var item: String? = null,
+
+    @ColumnInfo(name="jumlah")
+    var jumlah: String? = null,
+
+    @ColumnInfo(name="status")
+    var status: Int = 0,
+)
