9b15ec490bb0183457ba467b5874d54fc71d62de
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-12-07 01:10:08 +0700

    setup db + DAO

---
+++ app/src/main/java/paba/b/room/database/daftarBelanjaDAO.kt
@@ -0,0 +1,21 @@
+package paba.b.room.database
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface daftarBelanjaDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(daftar: daftarBelanja)
+
+    @Query("UPDATE daftarBelanja SET tanggal=:isi_tanggal, item=:isi_item, jumlah=:isi_jumlah WHERE id=:pilihid")
+    fun update(isi_tanggal: String, isi_item: String, isi_jumlah: String, isi_status: Int, pilihid: Int)
+
+    @Delete
+    fun delete(daftar: daftarBelanja)
+
+    @Query("SELECT * FROM daftarBelanja ORDER BY id asc")
+    fun selectAll() : MutableList<daftarBelanja>
+}
