3ea71a33acb2ab2f7cf1e03e8f8fc1b9a11cabc3
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-12-07 02:02:46 +0700

    create : tambah daftar view + controller

---
+++ app/src/main/java/paba/b/room/TambahDaftar.kt
@@ -0,0 +1,48 @@
+package paba.b.room
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import paba.b.room.database.daftarBelanja
+import paba.b.room.database.daftarBelanjaDB
+import paba.b.room.helper.DateHelper.getCurrentDate
+
+class TambahDaftar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tambah_daftar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var DB = daftarBelanjaDB.getDatabase(this)
+        var tanggal = getCurrentDate()
+
+        val _etItem = findViewById<EditText>(R.id.etItem)
+        val _etJumlah = findViewById<EditText>(R.id.etJumlah)
+        val _btnTambah = findViewById<Button>(R.id.btnTambah)
+        val _btnUpdate = findViewById<Button>(R.id.btnUpdate)
+
+        _btnTambah.setOnClickListener{
+            CoroutineScope(Dispatchers.IO).async {
+                DB.fundaftarBelanjaDAO().insert(
+                    daftarBelanja(
+                        tanggal = tanggal,
+                        item = _etItem.text.toString(),
+                        jumlah = _etJumlah.text.toString()
+                    )
+                )
+            }
+        }
+    }
+}
